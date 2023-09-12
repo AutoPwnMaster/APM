ef0171b4f792a7ce1d8134b4871e720698ec63b4
IceXinShou <41276723+IceLeiYu@users.noreply.github.com>
2023-09-12 16:39:15 +0800

    update port scanner

--- build_server.sh
+++ build_server.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 set -e # 如果有錯誤則離開
-if netstat -tuln | grep -q ":55553\b"; then # 如果伺服器已經開啟
+if lsof -i :55553; then # 如果伺服器已經開啟
     echo "PORT 55553 has been turned on"
 else
     echo "Building ... " 
     success=false # 預設為失敗
     msfrpcd -P $1 # 重頭戲 (開啟伺服器)
     for ((i = 0; i < 10; i += 1)); do # 嘗試 10 次檢測 PORT
-        if netstat -tuln | grep -q ":55553\b"; then
+        if lsof -i :55553; then
             success=true
             break
         fi
